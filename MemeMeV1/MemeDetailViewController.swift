148c016f9f730686a165c06a370ea356d0493a62
Francisco Robles <togusiluz@gmail.com>
2016-02-22 18:49:54 +0100

    Created MemeDetailView

---
+++ MemeMeV1/MemeDetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MemeDetailViewController.swift
+//  MemeMeV1
+//
+//  Created by Robles on 21/2/16.
+//  Copyright © 2016 Francisco Robles. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MemeDetailViewController: UIViewController {
+    
+    
+    
+    var meme : Meme!
+    
+    @IBOutlet weak var imageMeme: UIImageView!
+    
+}
